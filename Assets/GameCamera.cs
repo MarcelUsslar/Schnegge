e7e28364033d85af1c40891da2615a186ca82bc7
Adrian Khashaki <Adrian.Khashaki@kolibrigames.com>
2019-01-26 09:48:37 +0100

    Added Game Camera

---
+++ Assets/GameCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    private Transform _schneggeTransform;
+
+    private void Start()
+    {
+        _schneggeTransform = FindObjectOfType<Schnegge>().transform;
+    }
+
+    private void Update()
+    {
+        var pos = _schneggeTransform.position;
+
+        transform.position = new Vector3(pos.x, pos.y, -10);
+    }
+}
