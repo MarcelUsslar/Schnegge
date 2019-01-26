5544764f563096caa3631de59172653c6b10840c
Adrian Khashaki <Adrian.Khashaki@kolibrigames.com>
2019-01-26 01:10:11 +0100

    Implemented SoundService

---
+++ Assets/Schnegge.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class Schnegge : MonoBehaviour
+{
+    [UsedImplicitly] private void Start()
+    {
+        
+    }
+    
+    [UsedImplicitly] private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SoundService.PlaySound(Sound.Block);
+    }
+}
---
+++ Assets/Sound/SoundService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundService : MonoBehaviour
+{
+    [SerializeField] private AudioClip _mainMusic;
+    [SerializeField] private Tuple[] _audioClips;
+
+    private readonly List<AudioSource> _audioSources = new List<AudioSource>();
+    private static SoundService _instance;
+
+    [UsedImplicitly] private void Start()
+    {
+        _instance = this;
+        Play(_mainMusic, true);
+    }
+    
+    public static void PlaySound(Sound sound)
+    {
+        var sounds = _instance._audioClips.Where(tuple => tuple.Sound == sound).ToArray();
+        var randomSound = sounds[Random.Range(0, sounds.Length)];
+
+        _instance.Play(randomSound.AudioClip);
+    }
+
+    private void Play(AudioClip audioClip, bool loop = false)
+    {
+        var audioSource = GetAvailableAudioSource();
+
+        audioSource.clip = audioClip;
+        audioSource.Play(1);
+        audioSource.loop = loop;
+    }
+
+    private AudioSource GetAvailableAudioSource()
+    {
+        return _audioSources.Any(source => !source.isPlaying) 
+            ? _audioSources.First(source => !source.isPlaying) 
+            : ExpandSources();
+    }
+
+    private AudioSource ExpandSources()
+    {
+        var source = gameObject.AddComponent<AudioSource>();
+        _audioSources.Add(source);
+
+        return source;
+    }
+
+    [Serializable] private class Tuple
+    {
+        public AudioClip AudioClip;
+        public Sound Sound;
+    }
+}
