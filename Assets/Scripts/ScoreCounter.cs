883f3a45d3a6b1c4c70400d82797828cb4f56e0b
Marcel Usslar <marcel.usslar@kolibrigames.com>
2019-01-27 16:25:53 +0100

    Added foot spawner.

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -6,36 +6,47 @@ using TMPro;
 
 public class ScoreCounter : MonoBehaviour
 {
+    public static ScoreCounter Instance;
+
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TextMeshProUGUI textHighscore;
     [SerializeField] private GameObject schnegge;
     [SerializeField] private float scoreCountingSpeed = 10;
-    private int score = 0;
-    private int highscore = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int _score;
+    private int _highscore;
+
+    public int Score => _score;
+    public float ScoreCountingSpeed => scoreCountingSpeed;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        score = (int)(schnegge.transform.position.x * scoreCountingSpeed);
-        text.text = score.ToString();
+        _score = (int)(schnegge.transform.position.x * scoreCountingSpeed);
+        text.text = _score.ToString();
 
     }
 
     public void resetScore(){
         saveScore();
-        score = 0;
+        _score = 0;
     }
 
     private void saveScore(){
-        if(score > highscore){
-            highscore = score;
-            textHighscore.text = highscore.ToString();
+        if(_score > _highscore){
+            _highscore = _score;
+            textHighscore.text = _highscore.ToString();
             textHighscore.gameObject.SetActive(true);
         }
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
     }
 }
